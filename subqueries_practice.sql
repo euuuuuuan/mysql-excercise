af2e5dce56b0612ef2c9755ac046b22e37ab59e2
euan.may24@gmail.com <euan.may24@gmail.com>
2024-06-11 23:12:55 +0900

    additional commit

--- subqueries_practice.sql
+++ subqueries_practice.sql
@@ -12,15 +12,28 @@ where dept_id = (select dept_id
 
 
 -- EMPLOYEE 테이블에서 모든 직원의 평균 급여보다 높은 급여를 받는 직원의 이름과 급여를 조회하시오.
-# select emp_name,
-#        max(salary)
-# from employee a
-# where emp_name = (select max(salary)
-#                   from employee b
-#                   where a.emp_name = b.emp_name);
+select emp_name,
+       salary
+from employee
+where salary > (select avg(salary)
+                from employee);
 
 
 -- EMPLOYEE 테이블에서 각 부서의 평균 급여보다 높은 급여를 받는 직원의 이름, 부서명, 급여를 조회하시오.
+# select b.dept_title,
+#        a.emp_name,
+#        avg(salary)
+# from employee a
+#     left join department b on a.EMP_ID = b.DEPT_ID
+# group by emp_name, b.dept_title;
+
+# select emp_name,
+#        dept_code,
+#        avg(salary)
+# from employee
+# where avg(salary) >= 2000
+# group by emp_name, dept_code;
+# 그루핑을 하는 것과 안하는 것의 차이가 무엇인가. 위와 아래의 차이.
 
 
 -- 상관서브쿼리문제
@@ -31,9 +44,19 @@ where dept_id = (select dept_id
 
 -- EXISTS 절 문제
 -- DEPARTMENT 테이블에서 직원이 있는 부서의 부서명을 조회하시오.
-
+select a.dept_id,
+       a.dept_title
+from department a
+where exists(select dept_id
+             from department b
+             where b.dept_id = a.dept_id);
 -- EMPLOYEE 테이블에서 급여가 가장 높은 직원의 이름과 급여를 조회하시오. (NOT EXISTS 사용)
-
+# select a.emp_name,
+#        max(salary)
+# from employee a
+# where not exists(select salary
+#              from employee b
+#              where b.emp_name = a.emp_name);
 
 -- CTE 문제
 -- EMPLOYEE 테이블에서 각 부서별 평균 급여를 계산하고,
